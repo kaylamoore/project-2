7699da72754c27b5113fcdb24b68dade58f4245e
Kayla Moore <kaylamoore@Kaylas-MacBook-Pro-3.local>
2015-09-15 16:33:28 -0700

    creates references for notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915232438) do
+ActiveRecord::Schema.define(version: 20150915233245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20150915232438) do
     t.text     "list"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
 
   create_table "pictures", force: :cascade do |t|
     t.string   "title"
@@ -37,4 +40,5 @@ ActiveRecord::Schema.define(version: 20150915232438) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "notes", "users"
 end
